623ec69e839ca7733eae6ea8a47a857c6988ab07
Jeremy Mailen <jeremy@gladly.com>
2017-05-09 00:54:43 -0700

    Fix task property warning, show relative source path, upgrade kotlin

--- src/main/kotlin/org/jmailen/gradle/kotlinter/tasks/LintTask.kt
+++ src/main/kotlin/org/jmailen/gradle/kotlinter/tasks/LintTask.kt
@@ -4,16 +4,21 @@ import com.github.shyiko.ktlint.core.KtLint
 import com.github.shyiko.ktlint.core.RuleSet
 import org.gradle.api.GradleException
 import org.gradle.api.logging.LogLevel
+import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.SourceTask
 import org.gradle.api.tasks.TaskAction
 
 open class LintTask : SourceTask() {
+
+    @Input
     lateinit var ruleSets: List<RuleSet>
 
     @TaskAction
     fun run() {
         this.getSource().forEach { file ->
-            logger.log(LogLevel.DEBUG, "linting: $file")
+            val relativePath = file.toRelativeString(project.projectDir)
+
+            logger.log(LogLevel.DEBUG, "linting: $relativePath")
 
             val lintFunc = when (file.name.endsWith(".kt")) {
                 true -> KtLint::lint
@@ -23,7 +28,7 @@ open class LintTask : SourceTask() {
             var hasLintErrors = false
             lintFunc(file.readText(), ruleSets) { ruleError ->
                 logger.log(LogLevel.ERROR,
-                        "Lint error > ${file.path}:${ruleError.line}:${ruleError.col}: ${ruleError.detail}")
+                        "Lint error > $relativePath:${ruleError.line}:${ruleError.col}: ${ruleError.detail}")
                 hasLintErrors = true
             }
             if (hasLintErrors) {

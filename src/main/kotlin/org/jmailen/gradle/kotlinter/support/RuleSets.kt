9b2a21e5a3067f136510ef8a5643dce5167fd132
Jeremy Mailen <jeremy@gladly.com>
2017-05-08 23:06:13 -0700

    RuleSet resolution

---
+++ src/main/kotlin/org/jmailen/gradle/kotlinter/support/RuleSets.kt
@@ -0,0 +1,14 @@
+package org.jmailen.gradle.kotlinter.support
+
+import com.github.shyiko.ktlint.core.RuleSet
+import com.github.shyiko.ktlint.core.RuleSetProvider
+import java.util.ServiceLoader
+
+fun resolveRuleSets(providers: Iterable<RuleSetProvider> = ServiceLoader.load(RuleSetProvider::class.java)): List<RuleSet> {
+    return providers.map { it.get() }.sortedWith(compareBy {
+        when (it.id) {
+            "standard" -> 0
+            else -> 1
+        }
+    })
+}
---
+++ src/test/kotlin/org/jmailen/gradle/kotlinter/support/RuleSetsTest.kt
@@ -0,0 +1,41 @@
+package org.jmailen.gradle.kotlinter.support
+
+import com.github.shyiko.ktlint.core.Rule
+import com.github.shyiko.ktlint.core.RuleSet
+import com.github.shyiko.ktlint.core.RuleSetProvider
+import org.jetbrains.kotlin.com.intellij.lang.ASTNode
+import org.junit.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class RuleSetsTest {
+
+    @Test
+    fun `resolveRuleSets loads from classpath providers`() {
+        val result = resolveRuleSets()
+
+        assertEquals(1, result.size)
+        assertEquals("standard", result.first().id)
+    }
+
+    @Test
+    fun `resolveRuleSets puts standard rules first`() {
+        val standard = TestRuleSetProvider(RuleSet("standard", TestRule("one")))
+        val extra1 = TestRuleSetProvider(RuleSet("extra-one", TestRule("two")))
+        val extra2 = TestRuleSetProvider(RuleSet("extra-two", TestRule("three")))
+
+        val result = resolveRuleSets(listOf(extra2, standard, extra1))
+
+        assertEquals(3, result.size)
+        assertEquals(standard.ruleSet, result.first())
+        assertTrue(result.containsAll(listOf(extra1.ruleSet, extra2.ruleSet)))
+    }
+}
+
+class TestRuleSetProvider(val ruleSet: RuleSet) : RuleSetProvider {
+    override fun get() = ruleSet
+}
+
+class TestRule(id: String) : Rule(id) {
+    override fun visit(node: ASTNode, autoCorrect: Boolean, emit: (offset: Int, errorMessage: String, canBeAutoCorrected: Boolean) -> Unit) {}
+}

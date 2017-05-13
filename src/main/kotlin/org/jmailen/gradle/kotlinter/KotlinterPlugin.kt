51665d0dce42553fd499803974867bd4509ad1ec
Jeremy Mailen <jeremy@gladly.com>
2017-05-13 01:09:36 -0700

    Prevent plugin from being applied when kotlin plugin not present

--- src/main/kotlin/org/jmailen/gradle/kotlinter/KotlinterPlugin.kt
+++ src/main/kotlin/org/jmailen/gradle/kotlinter/KotlinterPlugin.kt
@@ -7,6 +7,7 @@ import org.gradle.api.file.SourceDirectorySet
 import org.gradle.api.internal.HasConvention
 import org.gradle.api.plugins.JavaPluginConvention
 import org.gradle.api.tasks.SourceSet
+import org.jetbrains.kotlin.gradle.plugin.KotlinPluginWrapper
 import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 import org.jmailen.gradle.kotlinter.support.resolveRuleSets
 import org.jmailen.gradle.kotlinter.tasks.FormatTask
@@ -16,14 +17,30 @@ class KotlinterPlugin : Plugin<Project> {
 
     override fun apply(project: Project?) {
         project?.let { p ->
-            createTasks(p)
+            p.plugins.withType(KotlinPluginWrapper::class.java) {
+
+                KotlinterApplier(p, p.kotlinSourceSets(), resolveRuleSets()).createTasks()
+            }
         }
     }
 
-    fun createTasks(project: Project) {
+    private fun Project.kotlinSourceSets() = sourceSets().map { it.kotlin }.filterNotNull()
+
+    private fun Project.sourceSets() = convention.getPlugin(JavaPluginConvention::class.java).sourceSets
+
+    private val SourceSet.kotlin: SourceDirectorySet?
+        get() = ((getConvention("kotlin") ?: getConvention("kotlin2js")) as? KotlinSourceSet)?.kotlin
+
+    private fun Any.getConvention(name: String): Any? =
+            (this as HasConvention).convention.plugins[name]
+}
+
+class KotlinterApplier(val project: Project, val kotlinSourceSets: List<SourceDirectorySet>, val ruleSets: List<RuleSet>) {
+
+    fun createTasks() {
         val ruleSets = resolveRuleSets()
 
-        val formatTasks = createFormatTasks(project, ruleSets)
+        val formatTasks = createFormatTasks(ruleSets)
 
         project.tasks.create("formatKotlin") {
             it.group = "formatting"
@@ -31,7 +48,7 @@ class KotlinterPlugin : Plugin<Project> {
             it.dependsOn(formatTasks)
         }
 
-        val lintTasks = createLintTasks(project, ruleSets)
+        val lintTasks = createLintTasks(ruleSets)
 
         val lintKotlin = project.tasks.create("lintKotlin") {
             it.group = "verification"
@@ -44,39 +61,29 @@ class KotlinterPlugin : Plugin<Project> {
         }
     }
 
-    fun createLintTasks(project: Project, ruleSets: List<RuleSet>) =
-            project.kotlinSourceSets().map { sourceSet ->
-                val taskName = "lintKotlin${sourceSet.id().capitalize()}"
-
-                project.tasks.create(taskName, LintTask::class.java) { task ->
-                    task.source(sourceSet.sourceDirectories.files)
-                    task.ruleSets = ruleSets
-                    task.report = project.reportFile("${sourceSet.id()}-lint.txt")
-                }
-            }
-
-    fun createFormatTasks(project: Project, ruleSets: List<RuleSet>) =
-            project.kotlinSourceSets().map { sourceSet ->
+    fun createFormatTasks(ruleSets: List<RuleSet>) =
+            kotlinSourceSets.map { sourceSet ->
                 val taskName = "formatKotlin${sourceSet.id().capitalize()}"
 
                 project.tasks.create(taskName, FormatTask::class.java) { task ->
                     task.source(sourceSet.sourceDirectories.files)
                     task.ruleSets = ruleSets
-                    task.report = project.reportFile("${sourceSet.id()}-format.txt")
+                    task.report = reportFile("${sourceSet.id()}-format.txt")
                 }
             }
+
+    fun createLintTasks(ruleSets: List<RuleSet>) =
+            kotlinSourceSets.map { sourceSet ->
+                val taskName = "lintKotlin${sourceSet.id().capitalize()}"
+
+                project.tasks.create(taskName, LintTask::class.java) { task ->
+                    task.source(sourceSet.sourceDirectories.files)
+                    task.ruleSets = ruleSets
+                    task.report = reportFile("${sourceSet.id()}-lint.txt")
+                }
+            }
+
+    private fun reportFile(name: String) = project.file("${project.buildDir}/reports/ktlint/$name")
+
+    private fun SourceDirectorySet.id() = name.split(" ").first()
 }
-
-fun Project.reportFile(name: String) = file("$buildDir/reports/ktlint/$name")
-
-fun Project.kotlinSourceSets() = sourceSets().map { it.kotlin }.filterNotNull()
-
-fun Project.sourceSets() = convention.getPlugin(JavaPluginConvention::class.java).sourceSets
-
-fun SourceDirectorySet.id() = name.split(" ").first()
-
-private val SourceSet.kotlin: SourceDirectorySet?
-    get() = ((getConvention("kotlin") ?: getConvention("kotlin2js")) as? KotlinSourceSet)?.kotlin
-
-internal fun Any.getConvention(name: String): Any? =
-        (this as HasConvention).convention.plugins[name]

1cb478036c37d8d84057e25054114d71082f32ed
Sam Zurcher <sam@ergon.ch>
2019-04-08 07:43:25 +0200

    Uses a thread safe construct for FormatExecutionContext.fixes

--- src/main/kotlin/org/jmailen/gradle/kotlinter/tasks/format/FormatExecutionContext.kt
+++ src/main/kotlin/org/jmailen/gradle/kotlinter/tasks/format/FormatExecutionContext.kt
@@ -2,11 +2,13 @@ package org.jmailen.gradle.kotlinter.tasks.format
 
 import org.gradle.api.logging.Logger
 import org.jmailen.gradle.kotlinter.support.ExecutionContext
+import java.util.Queue
+import java.util.concurrent.ConcurrentLinkedQueue
 
 /**
  * Execution context for the formatting task.
  */
 data class FormatExecutionContext(
     override val logger: Logger,
-    val fixes: MutableList<String> = mutableListOf()
+    val fixes: Queue<String> = ConcurrentLinkedQueue<String>()
 ) : ExecutionContext

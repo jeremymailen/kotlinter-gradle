6e3cd2339de1e77574d587fe0b6ace60b2721c9f
Drew Stephens <drew@dinomite.net>
2020-04-08 17:00:22 -0400

    Find gradlew automatically

--- src/main/kotlin/org/jmailen/gradle/kotlinter/support/Git.kt
+++ src/main/kotlin/org/jmailen/gradle/kotlinter/support/Git.kt
@@ -3,20 +3,25 @@ package org.jmailen.gradle.kotlinter.support
 import org.gradle.api.GradleException
 import java.io.File
 
-
 fun findGitDir(dir: File): File {
-    return findGitDirNullable(dir) ?: throw GradleException("Could not find .git directory; searched $dir and parents")
+    return findInParents(".git", dir)
+        ?: throw GradleException("Could not find .git directory; searched $dir and parents")
 }
 
-private tailrec fun findGitDirNullable(dir: File): File? {
-    val gitDir = File(dir, ".git")
+fun findGradlew(dir: File): File {
+    return findInParents("gradlew", dir)
+        ?: throw GradleException("Could not find .git directory; searched $dir and parents")
+}
+
+private tailrec fun findInParents(toFind: String, startFrom: File): File? {
+    val gitDir = File(startFrom, toFind)
     if (gitDir.exists()) {
         return gitDir
     }
 
-    if (dir.parentFile == null || !dir.parentFile.exists()) {
+    if (startFrom.parentFile == null || !startFrom.parentFile.exists()) {
         return null
     }
 
-    return findGitDirNullable(dir.parentFile)
+    return findInParents(toFind, startFrom.parentFile)
 }
--- src/main/kotlin/org/jmailen/gradle/kotlinter/tasks/InstallPrePushHookTask.kt
+++ src/main/kotlin/org/jmailen/gradle/kotlinter/tasks/InstallPrePushHookTask.kt
@@ -3,6 +3,7 @@ package org.jmailen.gradle.kotlinter.tasks
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 import org.jmailen.gradle.kotlinter.support.findGitDir
+import org.jmailen.gradle.kotlinter.support.findGradlew
 import java.io.File
 
 open class InstallPrePushHookTask : DefaultTask() {
@@ -10,6 +11,7 @@ open class InstallPrePushHookTask : DefaultTask() {
     fun run() {
         val dotGitDir = findGitDir(project.rootDir)
         logger.info(".git directory: $dotGitDir")
+        val gradlew = findGradlew(project.rootDir).path
 
         val hookDir = File(dotGitDir.absolutePath, "hooks")
         if (!hookDir.exists()) {
@@ -28,7 +30,7 @@ open class InstallPrePushHookTask : DefaultTask() {
 
         if (prePushHookFile.length() == 0L) {
             logger.info("Writing hook to empty file")
-            prePushHookFile.writeText(generateHook(addShebang = true))
+            prePushHookFile.writeText(generateHook(gradlew, addShebang = true))
             return
         }
 
@@ -37,10 +39,10 @@ open class InstallPrePushHookTask : DefaultTask() {
         if (startIndex == -1) {
             logger.info("Replacing existing hook")
             val endIndex = prePushHookFileContent.indexOf(endHook)
-            prePushHookFileContent.replaceRange(startIndex, endIndex, generateHook(includeEndHook = false))
+            prePushHookFileContent.replaceRange(startIndex, endIndex, generateHook(gradlew, includeEndHook = false))
         } else {
             logger.info("Appending hook to end of existing non-empty file")
-            prePushHookFile.appendText(generateHook())
+            prePushHookFile.appendText(generateHook(gradlew))
         }
     }
 
@@ -54,26 +56,27 @@ open class InstallPrePushHookTask : DefaultTask() {
             set -e
         """
 
-        // TODO workdir should be project root
         private const val prePushHook = """
-            GRADLE=./gradlew
-            LINT_COMMAND="${'$'}GRADLE lintKotlin"
-            FORMAT_COMMAND="${'$'}GRADLE formatKotlin"
-
-            ${'$'}LINT_COMMAND
+            ${'$'}GRADLEW lintKotlin
 
             status=${'$'}?
             if [ "${'$'}status" != 0 ] ; then
-                echo 1>&2 "\n${'$'}LINT_COMMAND found problems; running ${'$'}FORMAT_COMMAND..."
-                ${'$'}FORMAT_COMMAND
+                echo 1>&2 "\nLinting found problems; running format command..."
+                ${'$'}GRADLEW formatKotlin
                 exit 1
             fi
         """
 
-        private fun generateHook(addShebang: Boolean = false, includeEndHook: Boolean = true): String {
+        private fun generateHook(
+            gradlew: String,
+            addShebang: Boolean = false,
+            includeEndHook: Boolean = true
+        ): String {
             return """
                     ${if (addShebang) shebang else ""}
                     $startHook
+                    GRADLEW=$gradlew
+
                     $prePushHook
                     ${if (includeEndHook) endHook else ""}
                 """.trimIndent()

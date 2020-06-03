d6c768b60530b51f6d3be303f747354927a37a24
olegivo <olegivo@gmail.com>
2020-06-03 11:43:28 +0300

    update ktlint (0.37.0), kotlin (1.3.70)

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,9 +2,9 @@ object Versions {
     const val androidTools = "3.5.3"
     const val jetbrainsAnnotations = "18.0.0"
     const val junit = "4.13"
-    const val kotlin = "1.3.60"
+    const val kotlin = "1.3.70"
     const val kotlinter = "2.1.3"
-    const val ktlint = "0.36.0"
+    const val ktlint = "0.37.0"
     const val mockitoKotlin = "2.2.0"
     const val pluginPublish = "0.10.1"
 }

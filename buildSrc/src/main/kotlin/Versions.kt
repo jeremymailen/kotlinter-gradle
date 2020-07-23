1dd36a2fd19f72d9c6c96db195b17bbba8fd37dd
Drew Stephens <drew@dinomite.net>
2020-07-23 18:49:28 -0400

    Use latest Kotlinter version (#150)

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -3,7 +3,7 @@ object Versions {
     const val jetbrainsAnnotations = "18.0.0"
     const val junit = "4.13"
     const val kotlin = "1.3.72"
-    const val kotlinter = "2.4.0"
+    const val kotlinter = "2.4.1"
     const val ktlint = "0.37.2"
     const val mockitoKotlin = "2.2.0"
     const val pluginPublish = "0.12.0"

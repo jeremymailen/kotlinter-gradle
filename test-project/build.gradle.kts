12c35c60e983cf725f5f77dbd7b22614cd050590
Mateusz Kwieciński <mateusz.kwiatek@gmail.com>
2021-05-02 20:25:56 +0200

    Revert "Backport to pre gradle 6.8 solution"
    This reverts commit 6510e03161742ce9e2cfb94f520ed24e0f2f1a64.

--- test-project/build.gradle.kts
+++ test-project/build.gradle.kts
@@ -1,26 +1,4 @@
-buildscript {
-    repositories {
-        gradlePluginPortal {
-            content {
-                excludeGroup("org.jmailen.gradle")
-            }
-        }
-        if (project.hasProperty("useMavenLocal")) {
-            mavenLocal()
-        }
-    }
-    dependencies {
-        if (project.hasProperty("useMavenLocal")) {
-            classpath("org.jmailen.gradle.local:kotlinter-gradle:+")
-        } else {
-            classpath("org.jmailen.gradle:kotlinter-gradle")
-        }
-    }
-}
-
 plugins {
     kotlin("jvm") version "1.5.0"
+    id("org.jmailen.kotlinter")
 }
-
-apply(plugin = "org.jmailen.kotlinter")
-repositories.mavenCentral()

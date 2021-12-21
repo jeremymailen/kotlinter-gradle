e22fbe33e365091dd407cb942d7be99ccaced728
Jeremy Mailen <jeremy@divebell.com>
2021-12-20 16:08:11 -0800

    Release 3.8.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ val githubUrl = "https://github.com/jeremymailen/kotlinter-gradle"
 val webUrl = "https://github.com/jeremymailen/kotlinter-gradle"
 val projectDescription = "Lint and formatting for Kotlin using ktlint with configuration-free setup on JVM and Android projects"
 
-version = "3.7.0"
+version = "3.8.0"
 group = "org.jmailen.gradle"
 description = projectDescription
 

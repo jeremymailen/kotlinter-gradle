9931d2965ef076100ea2a0bf1f42dfc66d320172
Mateusz Kwieciński <36954793+mateuszkwiecinski@users.noreply.github.com>
2022-06-26 11:55:30 +0200

    Prepare release 3.11.1 (#261)

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ val githubUrl = "https://github.com/jeremymailen/kotlinter-gradle"
 val webUrl = "https://github.com/jeremymailen/kotlinter-gradle"
 val projectDescription = "Lint and formatting for Kotlin using ktlint with configuration-free setup on JVM and Android projects"
 
-version = "3.11.0"
+version = "3.11.1"
 group = "org.jmailen.gradle"
 description = projectDescription
 

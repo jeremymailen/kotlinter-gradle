dac444598300fdbad4c6d91fabf4f4ed79e7e285
Goooler <wangzongler@gmail.com>
2022-08-20 00:04:04 +0800

    Use the correct source directory set for KGP

--- src/main/kotlin/org/jmailen/gradle/kotlinter/SourceSetApplier.kt
+++ src/main/kotlin/org/jmailen/gradle/kotlinter/SourceSetApplier.kt
@@ -1,11 +1,17 @@
 package org.jmailen.gradle.kotlinter
 
+import org.gradle.api.NamedDomainObjectContainer
 import org.gradle.api.Project
 import org.gradle.api.file.FileTree
 import org.gradle.api.provider.Provider
+import org.jetbrains.kotlin.gradle.dsl.KotlinProjectExtension
+import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 
 internal typealias SourceSetAction = (String, Provider<FileTree>) -> Unit
 
 interface SourceSetApplier {
     fun applyToAll(project: Project, action: SourceSetAction)
+
+    val Project.kotlinSourceSets: NamedDomainObjectContainer<KotlinSourceSet>
+        get() = project.extensions.getByType(KotlinProjectExtension::class.java).sourceSets
 }
--- src/main/kotlin/org/jmailen/gradle/kotlinter/pluginapplier/KotlinJvmSourceSetApplier.kt
+++ src/main/kotlin/org/jmailen/gradle/kotlinter/pluginapplier/KotlinJvmSourceSetApplier.kt
@@ -1,9 +1,6 @@
 package org.jmailen.gradle.kotlinter.pluginapplier
 
-import org.gradle.api.NamedDomainObjectContainer
 import org.gradle.api.Project
-import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
-import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 import org.jmailen.gradle.kotlinter.SourceSetAction
 import org.jmailen.gradle.kotlinter.SourceSetApplier
 import org.jmailen.gradle.kotlinter.id
@@ -11,13 +8,10 @@ import org.jmailen.gradle.kotlinter.id
 internal object KotlinJvmSourceSetApplier : SourceSetApplier {
 
     override fun applyToAll(project: Project, action: SourceSetAction) {
-        getSourceSets(project).configureEach { sourceSet ->
+        project.kotlinSourceSets.configureEach { sourceSet ->
             sourceSet.kotlin.let { directorySet ->
                 action(directorySet.name.id, project.provider { directorySet })
             }
         }
     }
-
-    private fun getSourceSets(project: Project): NamedDomainObjectContainer<KotlinSourceSet> =
-        project.extensions.findByType(KotlinJvmProjectExtension::class.java)!!.sourceSets
 }

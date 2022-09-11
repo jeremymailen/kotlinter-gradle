c10f80046672812053968161d913ff89fdd2aea4
Mateusz Kwieciński <mateusz.kwiatek@gmail.com>
2022-09-11 14:22:55 +0200

    Ensure `compileOnly` classpath and default ktlint runtime are kept in sync

--- src/main/kotlin/org/jmailen/gradle/kotlinter/support/VersionProperties.kt
+++ src/main/kotlin/org/jmailen/gradle/kotlinter/support/VersionProperties.kt
@@ -2,10 +2,14 @@ package org.jmailen.gradle.kotlinter.support
 
 import java.util.Properties
 
-class VersionProperties : Properties() {
+internal val versionProperties by lazy(::VersionProperties)
+
+internal class VersionProperties : Properties() {
     init {
         load(this.javaClass.getResourceAsStream("/version.properties"))
     }
 
-    fun version(): String = getProperty("version")
+    fun kotlinterVersion(): String = getProperty("version")
+
+    fun ktlintVersion(): String = getProperty("ktlint_version")
 }

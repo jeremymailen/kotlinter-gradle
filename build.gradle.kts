04ec340589f20b1a470d6fba005444cc62998b1a
Mateusz Kwieciński <mateusz.kwiatek@gmail.com>
2022-10-01 11:27:16 +0200

    Bump kotlin to 1.7.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.plugin.getKotlinPluginVersion
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.10"
+    kotlin("jvm") version "1.7.20"
     id("com.gradle.plugin-publish") version "0.21.0"
     `java-gradle-plugin`
     `maven-publish`

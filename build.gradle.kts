1c3c537f529699fdd013ccced3539aa3f98625df
Jeremy Mailen <jeremy@opsinsecurity.com>
2025-05-23 12:09:28 -0700

    Release 5.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ val githubUrl = "https://github.com/jeremymailen/kotlinter-gradle"
 val webUrl = "https://github.com/jeremymailen/kotlinter-gradle"
 val projectDescription = "Lint and formatting for Kotlin using ktlint with configuration-free setup on JVM and Android projects"
 
-version = "5.0.2"
+version = "5.1.0"
 group = "org.jmailen.gradle"
 description = projectDescription
 
